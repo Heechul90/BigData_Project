47c02800e9c9528fa7dd3f3a1cd62c4b5b3db303
Heechul90 <heechul4296@gmail.com>
2019-10-31 11:01:01 +0900

    전처리

--- 기상데이터 전처리.py
+++ 기상데이터 전처리.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # 데이터 불러오기
 
-raw_data = pd.read_csv('Data/2014-2018 시간별 기상데이터.csv',
+raw_data = pd.read_csv('Data/20191031095811.csv',
                        encoding = 'euc-kr')
 raw_data.head()
 raw_data.tail()
@@ -13,27 +13,36 @@ raw_data.tail()
 data = raw_data.copy()
 data.head()
 data.columns
-data.rename(columns = {'일시': 'date',
-                       '평균기온(°C)': 'temp',
-                       '최저기온(°C)': 'lowest temp',
-                       '최고기온(°C)': 'highest temp',
-                       '일강수량(mm)': 'rainfall',
-                       '평균 풍속(m/s)': 'wind speed',
-                       '평균 이슬점온도(°C)': 'dew point',
-                       '평균 상대습도(%)': 'humidity',
-                       '합계 일조 시간(hr)': 'sunshine',
-                       '일 최심적설(cm)': 'snowfall'},
+data.rename(columns = {'일시': '일시',
+                       '평균기온(°C)': '',
+                       '최저기온(°C)': '',
+                       '최고기온(°C)': '',
+                       '일강수량(mm)': '',
+                       '최대 풍속(m/s)': '',
+                       '평균 풍속(m/s)': '',
+                       '최다풍향(16방위)': '',
+                       '평균 이슬점온도(°C)': '',
+                       '평균 상대습도(%)': '',
+                       '평균 현지기압(hPa)': '',
+                       '평균 해면기압(hPa)': '',
+                       '합계 일조시간(hr)': '',
+                       '합계 일사량(MJ/m2)': '',
+                       '일 최심적설(cm)': '',
+                       '평균 전운량(1/10)': '',
+                       '평균 중하층운량(1/10)': '',
+                       '평균 지면온도(°C)': ''},
             inplace = True)
 data.head()
+data.dtypes
 
 year = []
 month = []
 day = []
 
-for i in range(len(data['date'])):
-    year.append(data['date'].loc[i][:4])
-    month.append(data['date'].loc[i][5:7])
-    day.append(data['date'].loc[i][8:])
+for i in range(len(data['일시'])):
+    year.append(data['일시'].loc[i][:4])
+    month.append(data['일시'].loc[i][5:7])
+    day.append(data['일시'].loc[i][8:])
 
 year
 month
@@ -44,45 +53,76 @@ data['month'] = month
 data['day'] = day
 data.head()
 
-del data['date']
+del data['일시']
 data.head()
 data.columns
 data = pd.DataFrame(data, columns = ['year', 'month', 'day',
-                                     'temp', 'lowest temp', 'highest temp',
-                                     'rainfall', 'wind speed', 'dew point',
-                                     'humidity', 'sunshine', 'snowfall'])
+                                     '평균기온(°C)', '최저기온(°C)', '최고기온(°C)', '일강수량(mm)', '최대 풍속(m/s)',
+                                     '평균 풍속(m/s)', '최다풍향(16방위)', '평균 이슬점온도(°C)', '평균 상대습도(%)',
+                                     '평균 현지기압(hPa)', '평균 해면기압(hPa)', '합계 일조시간(hr)', '합계 일사량(MJ/m2)',
+                                     '일 최심적설(cm)', '평균 전운량(1/10)', '평균 중하층운량(1/10)', '평균 지면온도(°C)'])
 data.head()
 data.columns
+data.dtypes
 data['year'].unique()
 data['month'].unique()
 data['day'].unique()
 
-data['temp'].unique()          # 평균으로
-data['temp'].fillna(data['temp'].mean(), inplace = True)
+data['평균기온(°C)'].unique()          # 평균으로
 
-data['lowest temp'].unique()   # 평균으로```
-data['lowest temp'].fillna(data['lowest temp'].mean(), inplace = True)
 
-data['highest temp'].unique()  # 평균으로
-data['highest temp'].fillna(data['highest temp'].mean(), inplace = True)
+data['최저기온(°C)'].unique()   # 평균으로```
 
-data['rainfall'].unique()      # 0값으로
-data['rainfall'] = data['rainfall'].fillna(0)
 
-data['wind speed'].unique()    # 평균으로
-data['wind speed'].fillna(data['wind speed'].mean(), inplace = True)
+data['최고기온(°C)'].unique()  # 평균으로
 
-data['dew point'].unique()     # 평균으로
-data['dew point'].fillna(data['dew point'].mean(), inplace = True)
 
-data['humidity'].unique()      # 평균으로
-data['humidity'].fillna(data['humidity'].mean(), inplace = True)
+data['일강수량(mm)'].unique()      # 0값으로
+data['일강수량(mm)'] = data['일강수량(mm)'].fillna(0)
 
-data['sunshine'].unique()      # 평균으로
-data['sunshine'].fillna(data['sunshine'].mean(), inplace = True)
 
-data['snowfall'].unique()      # 0값으로
-data['snowfall'] = data['snowfall'].fillna(0)
+data['최대 풍속(m/s)'].unique()    # 평균으로
+
+
+data['평균 풍속(m/s)'].unique()     # 평균으로
+
+
+data['최다풍향(16방위)'].unique()      # 평균으로
+data['최다풍향(16방위)'].fillna(data['최다풍향(16방위)'].mean(), inplace = True)
+
+
+data['평균 이슬점온도(°C)'].unique()
+
+
+data['평균 상대습도(%)'].unique()      # 평균으로
+data['평균 상대습도(%)'].fillna(data['평균 상대습도(%)'].mean(), inplace = True)
+
+
+data['평균 현지기압(hPa)'].unique()
+
+
+data['평균 해면기압(hPa)'].unique()
+
+
+data['합계 일조시간(hr)'].unique()
+
+
+data['합계 일사량(MJ/m2)'].unique()
+
+
+data['일 최심적설(cm)'].unique()      # 0으로
+data['일 최심적설(cm)'].fillna(0, inplace = True)
+
+
+data['평균 전운량(1/10)'].unique()
+
+
+data['평균 중하층운량(1/10)'].unique()      # 평균으로
+data['평균 중하층운량(1/10)'].fillna(data['평균 중하층운량(1/10)'].mean(), inplace = True)
+
+
+data['평균 지면온도(°C)'].unique()
+
 
 data.to_csv('Data/2014-2018 시간별 기상데이터1.csv',
             index = False,
---
+++ 전력량 예측 전처리5(딥러닝수요예측).py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+# 선형 회귀
+from keras.models import Sequential  # 딥러닝을 구동하는 데 필요한 케라스 함수를 불러옵니다.
+from keras.layers import Dense
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
+
+import numpy as np                # 필요한 라이브러리를 불러옵니다.
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 한글 사용하기
+import platform
+import matplotlib.pyplot as plt
+
+path = "c:/Windows/Fonts/malgun.ttf"
+from matplotlib import font_manager, rc
+
+if platform.system() == 'Darwin':
+    rc('font', family='AppleGothic')
+elif platform.system() == 'Windows':
+    font_name = font_manager.FontProperties(fname=path).get_name()
+    rc('font', family=font_name)
+else:
+    print('Unknown system... sorry~~~~')
+
+plt.rcParams['axes.unicode_minus'] = False
+
+
+
+# 데이터 불러오기
+dataset = pd.read_csv('Data/dataset2(watt).csv',
+                      index_col = 0,
+                      encoding = 'euc-kr')
+
+dataset.head()
+data = dataset.copy()
+
+
